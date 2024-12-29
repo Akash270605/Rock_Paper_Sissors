db1d20aa17a29ac2f60657745e7bd7f4a5ba32f2
Akash Kumar <akashkr270605@gmail.com>
2024-12-30 00:32:06 +0530

    Add files via upload

---
+++ Rock_Paper_Sissor/src/rock_paper_sissor/Main.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rock_paper_sissor;
+
+public class Main { 
+    public static void main(String[] args) {
+       
+        Game.panel_game();
+        Game.panel_introduction();
+    }   
+}
